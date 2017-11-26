273704a994756ac5875ea1755df8cc46b528264c
zhupa <382211145@qq.com>
2017-11-26 13:01:06 +0800

    提交

--- LTM.School/Controllers/HomeController.cs
+++ LTM.School/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using LTM.School.Models;
+using LTM.School.Application.ViewModels;
 
 namespace LTM.School.Controllers
 {
